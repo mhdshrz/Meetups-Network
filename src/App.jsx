30974758195f29d6b18869dc19fe109850647039
Mahdi Shirazi <mahdielmaximo@gmail.com>
2023-06-07 15:03:12 +0330

    Add UI component Card, add Layout component and changed App.jsx based on that.

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import MainNavigation from "./components/layout/MainNavigation/MainNavigation";
 import AllMeetupsPage from "./pages/AllMeetupsPage";
 import NewMeetupPage from "./pages/NewMeetupPage";
 import FavoritesPage from "./pages/FavoritesPage";
+import Layout from "./components/layout/Layout/Layout";
 
 function App() {
 	return (
 		<>
-			<MainNavigation />
-			<Routes>
-				<Route path="/" element={<AllMeetupsPage />} />
-				<Route path="/new-meetup" element={<NewMeetupPage />} />
-				<Route path="/favorites" element={<FavoritesPage />} />
-			</Routes>
+			<Layout>
+				<Routes>
+					<Route path="/" element={<AllMeetupsPage />} />
+					<Route path="/new-meetup" element={<NewMeetupPage />} />
+					<Route path="/favorites" element={<FavoritesPage />} />
+				</Routes>
+			</Layout>
 		</>
 	);
 }
