898de6acf6494e74025513fbfd58f691ef5baa7e
Mahdi Shirazi <mahdielmaximo@gmail.com>
2023-06-07 15:54:02 +0330

    Complete the AddNewMeetup form, sending POST requests to the API and posting data to our Firebase database.

--- src/components/meetups/NewMeetupForm/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm/NewMeetupForm.jsx
@@ -1,26 +1,55 @@
+import { useRef } from "react";
 import Card from "../../ui/Card/Card";
 
 import styles from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+	const titleInputRef = useRef();
+	const imageInputRef = useRef();
+	const addressInputRef = useRef();
+	const descriptionInputRef = useRef();
+
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		const enteredTitle = titleInputRef.current.value;
+		const enteredImage = imageInputRef.current.value;
+		const enteredAddress = addressInputRef.current.value;
+		const eneterdDescription = descriptionInputRef.current.value;
+
+		const meetup = {
+			title: enteredTitle,
+			image: enteredImage,
+			address: enteredAddress,
+			description: eneterdDescription,
+		};
+
+		props.onAddNewMeetup(meetup);
+	}
+
 	return (
 		<Card>
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit}>
 				<div className={styles.control}>
 					<label htmlFor="title">Meetup Title</label>
-					<input id="title" type="text" required />
+					<input id="title" type="text" required ref={titleInputRef} />
 				</div>
 				<div className={styles.control}>
 					<label htmlFor="image">Meetup Image</label>
-					<input id="image" type="url" required />
+					<input id="image" type="url" required ref={imageInputRef} />
 				</div>
 				<div className={styles.control}>
 					<label htmlFor="address">Meetup Address</label>
-					<input id="address" type="text" required />
+					<input id="address" type="text" required ref={addressInputRef} />
 				</div>
 				<div className={styles.control}>
 					<label htmlFor="description">Description</label>
-					<textarea id="description" rows="5" required></textarea>
+					<textarea
+						id="description"
+						rows="5"
+						required
+						ref={descriptionInputRef}
+					></textarea>
 				</div>
 				<div className={styles.actions}>
 					<button>Add</button>
--- src/pages/NewMeetupPage.jsx
+++ src/pages/NewMeetupPage.jsx
@@ -1,10 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm/NewMeetupForm";
 
 function NewMeetupPage() {
+	const navigate = useNavigate();
+
+	function handleAddNewMeetup(meetup) {
+		fetch(
+			"https://react-maxi-second-project-back-default-rtdb.firebaseio.com/meetups.json",
+			{
+				method: "POST",
+				body: JSON.stringify(meetup),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		).then(() => navigate("/", { replace: true }));
+	}
+
 	return (
 		<section>
 			<h1>Add New Meetups</h1>
-			<NewMeetupForm />
+			<NewMeetupForm onAddNewMeetup={handleAddNewMeetup} />
 		</section>
 	);
 }
